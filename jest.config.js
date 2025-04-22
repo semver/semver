7e7a04e26daf98c93902d9c837258d2aceb9f142
shan1919 <186241534+sangshanrupesh@users.noreply.github.com>
2025-04-22 03:14:52 -0400

    Add build and test scripts
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/semver/semver?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+};
